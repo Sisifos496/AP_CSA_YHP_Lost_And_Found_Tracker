5719879d3d4fb58cd08423743c0c9bc310d5fa34
Sisifos496 <116097380+Sisifos496@users.noreply.github.com>
2026-04-20 02:29:08 +0300

    final fixes never mind pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/AP_CSA_YHP_Lost_And_Found_Tracker/',
 })
